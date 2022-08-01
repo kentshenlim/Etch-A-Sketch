e5cf8db1b695eadf7d537edea62a000a86cd6c94
LessSmartKent <kentshen_lim24@hotmail.com>
2022-08-01 12:03:25 +0100

    Add functionaliy draw while holding mouse!

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 const pixelArray = document.querySelectorAll(".item");
+let mouseDownOrNot = false;
+const drawingRegion = document.querySelector(".container.big");
 
+pixelArray.forEach(pixel => {
+    pixel.addEventListener('mousedown', () => {
+        mouseDownOrNot = true;
+    })
+    pixel.addEventListener('mouseup', () => {
+        mouseDownOrNot = false;
+    })
+})
+
+pixelArray.forEach(pixel => {
+    pixel.addEventListener('mouseenter', () => {
+        if (mouseDownOrNot) {
+            pixel.classList.add('plotted');
+        }
+    })
+})
+
+
+
+
+
+
+
+/*
+pixelArray.forEach(pixel => {
+    pixel.addEventListener('mousedown', () => {
+        pixel.addEventListener('mouseenter', () => {
+            pixel.classList.add('plotted');
+        })
+    })
+})*/
